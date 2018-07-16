d6067cf7afe2d17213b53794e6c0fa354dd49552
Ken Hoff <ken@hoff.tech>
2018-07-16 15:04:35 -0600

    stream client singleton, checking for feed tokens, slugs, and IDs

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import stream from "getstream";
+let streamClient;
 
-class StreamReactComponents extends React.Component {
+class Feed extends React.Component {
+    constructor(props) {
+        super(props);
+        if (!this.props.feedToken) {
+            throw new Error("`feedToken` prop is required.");
+        }
+        if (!this.props.feedSlug) {
+            throw new Error("`feedSlug` prop is required.");
+        }
+        if (!this.props.feedID) {
+            throw new Error("`feedID` prop is required.");
+        }
+        this.feed = streamClient.feed(this.props.feedSlug, this.props.feedID, this.props.feedToken);
+        this.state = {
+            activities: []
+        };
+    }
+    componentDidMount() {
+        // make call to stream API, load activities into state
+        this
+            .feed
+            .get()
+            .then(response => {
+                this.setState({activities: response.results});
+            });
+    }
     render() {
-        return (<div>awesome stream react components</div>)
+        return <div>{
+                this
+                    .state
+                    .activities
+                    .map((activity) => {
+                        return (<div key={activity.id}>{activity.id}</div>);
+                    })
+            }</div>;
     }
 }
 
-export default StreamReactComponents;
+export default streamKey => {
+    // singletonning stream client with JS closure
+    if (!streamClient) {
+        streamClient = stream.connect(streamKey);
+    }
+    return {Feed};
+};
