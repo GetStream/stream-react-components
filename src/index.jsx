90cd806171006e8f24471fbcf2a9eab2f3377eba
Ken Hoff <ken@hoff.tech>
2018-07-16 12:02:19 -0600

    index

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class StreamReactComponents extends React.Component {
+    render() {
+        return (<div>awesome stream react components</div>)
+    }
+}
+
+export default StreamReactComponents;
