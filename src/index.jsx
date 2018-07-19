869a0d807fec325701873b5839ce4a45fbd7c8c8
Ken Hoff <ken@hoff.tech>
2018-07-19 13:28:47 -0600

    whoops, forgot to remove 'this' line

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,10 @@ import Feed from "./Feed.jsx";
 
 let streamClient;
 
-export default streamKey => {
+export default(streamKey) => {
     // singletonning stream client with JS closure
     if (!streamClient) {
         streamClient = stream.connect(streamKey);
-        this.streamClient = streamClient;
     }
     return {Feed: Feed(streamClient)};
 };
