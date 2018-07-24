92824c747e43aa269c67316656abddc9da9f64d4
Ken Hoff <ken@hoff.tech>
2018-07-24 16:12:18 -0600

    initial version of infinite scrolling

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Activity from "./Activity.jsx";
 import PropTypes from "prop-types";
+import Waypoint from "react-waypoint";
 
 export default(streamClient) => {
     return class Feed extends React.Component {
@@ -25,17 +26,25 @@ export default(streamClient) => {
             }
             this.feed = streamClient.feed(this.props.feedSlug, this.props.feedID, this.props.feedToken);
             this.state = {
-                activities: []
+                activities: [],
+                lastActivityID: null
             };
         }
         componentDidMount() {
+
+            this.setState({loading: true});
             // make call to stream API, load activities into state
             this.feed.get().then(response => {
-                this.setState({activities: response.results});
+                this.setState({
+                    loading: false,
+                    activities: response.results,
+                    lastActivityID: response.results[response.results.length - 1].id
+                });
             });
 
             // connect realtime feed
             this.feed.subscribe((data) => {
+                // todo - tweak this a little bit, as data might returned changed or removed activities
                 this.setState({
                     activities: [...data.new.concat(this.state.activities)]
                 });
@@ -44,7 +53,24 @@ export default(streamClient) => {
         componentWillUnmount() {
             this.feed.cancel();
         }
+        handleScrolledToBottom = () => {
+            this.setState({loading: true});
+            // make call to stream API, load activities into state
+            this.feed.get({id_lt: this.state.lastActivityID}).then(response => {
+                this.setState({
+                    activities: [
+                        ...this.state.activities,
+                        ...response.results
+                    ],
+                    lastActivityID: response.results[response.results.length - 1].id,
+                    loading: false
+                });
+            });
+        }
         render() {
+            let waypoint = this.state.loading
+                ? null
+                : (<Waypoint onEnter={this.handleScrolledToBottom}></Waypoint>);
             // custom renderer
             if (this.props.render) {
                 return this.props.render(this.state.activities);
@@ -55,14 +81,16 @@ export default(streamClient) => {
                         this.state.activities.map((activity) => {
                             return (<CustomActivityComponent activity={activity} key={activity.id}></CustomActivityComponent>);
                         })
-                    }
+                    }{waypoint}
                 </div>);
             } else {
                 return <div className="stream-react-components feed">{
                         this.state.activities.map((activity) => {
                             return (<Activity activity={activity} key={activity.id}></Activity>);
                         })
-                    }</div>;
+                    }
+                    {waypoint}
+                </div>;
             }
         }
     };
